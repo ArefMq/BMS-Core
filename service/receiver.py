66733d130fdd65120e491033991aa4afc61a776b
Aref Moqadam Mehr <arefmq@cafebazaar.ir>
2019-05-18 21:27:56 +0430

    fix indention

--- service/receiver.py
+++ service/receiver.py
@@ -10,49 +10,49 @@ DEBUG = True
 
 
 class Receiver:
-	def __init__(self, port=2222):
-		self.host = ''	# Symbolic name meaning all available interfaces
-		self.port = port	# Arbitrary non-privileged port
+    def __init__(self, port=2222):
+        self.host = ''    # Symbolic name meaning all available interfaces
+        self.port = port    # Arbitrary non-privileged port
 
-		# Datagram (udp) socket
-		try :
-			self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-			print('Socket created')
-		except socket.error, msg :
-			print('Failed to create socket. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
-			raise
+        # Datagram (udp) socket
+        try :
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            print('Socket created')
+        except socket.error, msg :
+            print('Failed to create socket. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+            raise
 
 
-		# Bind socket to local host and port
-		try:
-			self.socket.bind((self.host, self.port))
-			print('Socket bind complete.')
-		except socket.error , msg:
-			print('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
-			sys.exit()
-			
-		
-		self.db_connection = sqlite3.connect(DB_PATH)
-		self.db_connection_cursor = self.db_connection.cursor()
+        # Bind socket to local host and port
+        try:
+            self.socket.bind((self.host, self.port))
+            print('Socket bind complete.')
+        except socket.error , msg:
+            print('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+            sys.exit()
+            
+        
+        self.db_connection = sqlite3.connect(DB_PATH)
+        self.db_connection_cursor = self.db_connection.cursor()
     
     def destruct(self):
         self.socket.close()
         self.db_connection.close()
 
-	def recieve(self):
-		d = self.socket.recvfrom(1024)
-		data = d[0]
-		addr = d[1]
+    def recieve(self):
+        d = self.socket.recvfrom(1024)
+        data = d[0]
+        addr = d[1]
 
-		if not data: 
-			raise Exception('invalid data recieved.')
-		
-		a = bytearray(data.strip())
+        if not data: 
+            raise Exception('invalid data recieved.')
+        
+        a = bytearray(data.strip())
 
-		if DEBUG:
-			print('________________________________')
-			for i in range(14):
-				print(a[i])
+        if DEBUG:
+            print('________________________________')
+            for i in range(14):
+                print(a[i])
 
 
 if __name__ == "__main__":
