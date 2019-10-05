7de910042dc52909e165fc3993e199988a2ffb94
pi <me@example.com>
2019-10-05 18:48:17 +0100

    fix updater

--- updater.py
+++ updater.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from git import Repo, GitCommandError
 import subprocess, signal
+import os
 
 def kill_all_process():
     p = subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE)
@@ -10,14 +11,15 @@ def kill_all_process():
         line = str(line)
         if 'receiver.py' in line or 'manage.py' in line or 'sender.py' in line or 'homebridge' in line:
             pid = int(line.split()[1])
-            os.kill(pid, signal.SIGKILL)
+            os.system("sudo kill -9 %s" % str(pid))
+            #os.kill(pid, signal.SIGKILL)
 
 
 if __name__ == "__main__":
     try:
         g = Repo('.').git
         res = g.pull()
-        if res != 'Already up to date.':
+        if res != 'Already up-to-date.' and res != 'Already up to date.':
             print('updated:   :D\n%s' % res)
             kill_all_process()
             subprocess.call(['/home/pi/BMS-Core/after_update'])
